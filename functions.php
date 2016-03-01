d076ce1f42d4375a3cdcb4da405353a3407bafeb
Joe Bacal <bacalj@gmail.com>
2016-03-01 12:07:44 -0500

    removes post format support

--- functions.php
+++ functions.php
@@ -62,13 +62,13 @@ function obreezy_setup() {
 	 * Enable support for Post Formats.
 	 * See http://codex.wordpress.org/Post_Formats
 	 */
-	add_theme_support( 'post-formats', array(
-		'aside',
-		'image',
-		'video',
-		'quote',
-		'link',
-	) );
+	// add_theme_support( 'post-formats', array(
+	// 	'aside',
+	// 	'image',
+	// 	'video',
+	// 	'quote',
+	// 	'link',
+	// ) );
 
 	// Set up the WordPress core custom background feature.
 	add_theme_support( 'custom-background', apply_filters( 'obreezy_custom_background_args', array(
